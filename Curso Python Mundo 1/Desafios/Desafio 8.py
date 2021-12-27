611b416606823d1665c66625b7ceda9011440885
Daniel Oliveira <daniel_femorais@hotmail.com>
2021-12-27 10:14:45 -0300

    Curso em Video

---
+++ Curso Python Mundo 1/Desafios/Desafio 8.py
@@ -0,0 +1,13 @@
+#Aula 7 desafio 8
+#Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+
+medida = float(input('Uma distancia em metros: '))
+mm = medida * 1000
+cm = medida * 100
+dm = medida * 10
+dam = medida / 10
+hm = medida / 100
+km = medida / 1000
+print('a media  de  {}m coresponde a \n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm'.format(medida, km, hm, dam, dm, cm, mm))
+
+
